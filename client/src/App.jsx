a492fe3ba27d2fd88f344545a4e7499bf62bfbd7
Mohammad Rashed <mo2002mohammad@gmail.com>
2024-03-07 11:39:13 -0400

    minor changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
 
-  const [backendData, setBackendData] = useState([{}]);
+  const [backendData, setBackendData] = useState([]);
 
   useEffect(() => {
     fetch("/toptracks").then(
@@ -12,7 +12,7 @@ function App() {
     ).then(
       data => {
         setBackendData(data);
-        // console.log(data);
+        console.log(data);
       }
      
     )
@@ -23,30 +23,48 @@ function App() {
    */
  let tracks = [];
 
- backendData.forEach((track, index) => {
+//  backendData.forEach((track, index) => {
+//   tracks.push(
+//     <div key={index}>
+//       <h2>Track: {track.name}</h2>
+//       <h4>Album: {track.id}</h4>
+//       <a href={track.album.external_urls.spotify} target="new">
+//         <img src={track.album.images[0].url} alt=""/>
+//       </a>
+//       <audio controls>
+//         <source  src={track.preview_url}/>
+//       </audio>
+//     </div>
+//   );
+//  })
+
+backendData.map((track) =>
   tracks.push(
-    <div key={index}>
+    <div key={track.id}>
       <h2>Track: {track.name}</h2>
-      <h4>Album: {track.id}</h4>
+      <h4>Album: {track.album.name}</h4>
       <a href={track.album.external_urls.spotify} target="new">
-        <img src={track.album.images[0].url} alt=""/>
+        <img src={track.album.images[0].url} alt="" />
       </a>
-      <audio controls>
-        <source  src={track.preview_url}/>
+      <audio controls style={{ display: "block", margin: "0 auto" }}>
+        <source src={track.preview_url} type="audio/mpeg"/>
       </audio>
     </div>
-  );
- })
+  )
+);
+
+
+
 
   // console.log(backendData[0].album);
   return (
-    <div className='App'>
-      <header className='App-header'>
+    <div className="App">
+      <header className="App-header">
         <Login />
         {tracks}
       </header>
     </div>
-  )
+  );
 }
 
 export default App;
